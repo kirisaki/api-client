5e9397bfdb126cf9ae4fb955e72e2f971480f972
Ryusuke ENDO <endouryusuke@realglobe.jp>
2018-10-02 19:16:37 +0900

    Add tests

--- src/Network/Api.hs
+++ src/Network/Api.hs
@@ -8,12 +8,77 @@
 --
 -----------------------------------------------------------------------------
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Network.Api where
 
-import Data.Text
+import Data.Aeson
+import Data.ByteString as BS
+import Data.List as L
+import Data.Text as T
 import GHC.Generics
-import Network.HTTP.Client
-import Network.HTTP.Types
+import qualified Network.HTTP.Client as C
+
+call :: Request -> Service -> IO ByteString
+call req service = undefined
+
+buildRequest :: Request -> Method -> Service -> C.Request
+buildRequest req method service = undefined
+
+lookupMethod :: Request -> Service -> Maybe Method
+lookupMethod req service = undefined
 
 
-data ApiMethod = ApiMethod
+injectUrlParams :: Text -> [(Text, Text)] -> Either Text Text
+injectUrlParams path params =
+  let
+    replace (k, v) p =
+      if p == ':' `T.cons` k ||
+         p == '{' `T.cons` k `T.snoc` '}'
+      then v else p
+    replaceAll kv ps = L.map (replace kv) ps 
+  in
+    undefined
+
+data Method = Method
+  { httpMethod :: HttpMethod
+  , apiEndpoint :: Text
+  } deriving (Eq, Show, Ord, Read, Generic)
+instance FromJSON Method
+instance ToJSON Method
+
+data Service = Service
+  { baseUrl :: Text
+  , tokenUrl :: Maybe Text
+  , tokenGetter :: Maybe Method
+  , tokenRefresher :: Maybe Method
+  , methods :: [Method]
+  , defaultHeader :: [(Text, Text)]
+  , tokenHeaderName :: Maybe Text
+  , tokenQueryName :: Maybe Text
+  } deriving (Eq, Show, Ord, Read, Generic)
+instance FromJSON Service
+instance ToJSON Service
+
+data HttpMethod
+  = GET
+  | POST
+  | HEAD
+  | PUT
+  | DELETE
+  | TRACE
+  | CONNECT
+  | OPTIONS
+  | PATCH
+  | Custom Text
+  deriving (Show, Ord, Eq, Read, Generic)
+instance FromJSON HttpMethod
+instance ToJSON HttpMethod
+
+data Request = Request
+  { requestMethod :: HttpMethod
+  , requestPath :: Text
+  , pathParams :: [(Text, Text)]
+  , queryParams :: [(Text, Text)]
+  , headerParams :: [(Text, Text)]
+  , requestBody :: ByteString
+  } deriving (Eq, Show, Ord, Read, Generic)
--- test/Network/ApiSpec.hs
+++ test/Network/ApiSpec.hs
@@ -1,9 +1,89 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Network.ApiSpec where
 
-import Test.Hspec 
+import Test.Hspec
+import Network.Api
 
 spec :: Spec
 spec = do
-  describe "offer" $ do
-    it "because of radical changes" $ do
-      1 `shouldBe` 1
+  describe "injectUrl" $ do
+    it "path with colon parameters 1" $ do
+      injectUrlParams "/user/:id" [("id", "1234")] `shouldBe` Right "/user/1234"
+    it "path with colon parameters 2" $ do
+      injectUrlParams "/user/:id/comment/:num" [("id", "42"), ("num", "3")] `shouldBe` Right "/user/42/comment/3"
+    it "path with braced parameters" $ do
+      injectUrlParams "/user/{id}/comment/{num}" [("id", "42"), ("num", "3")] `shouldBe` Right "/user/42/comment/3"
+    it "mixed parameters" $ do
+        injectUrlParams "/user/{id}/comment/:num" [("id", "42"), ("num", "3")] `shouldBe` Right "/user/42/comment/3"
+    it "lack parameters" $ do
+      injectUrlParams "/user/:id/comment/:num" [("id", "42")] `shouldBe` Left "lack parameters"
+    it "too much parameters" $ do
+      injectUrlParams "/user/{id}" [("id", "1234"), ("nyaan", "hoge")] `shouldBe` Left "too much parameters"
+
+  describe "lookupMethod" $ do
+    let service
+          = Service
+            "https://example.net"
+            Nothing
+            Nothing
+            Nothing
+            [ Method GET "user/:id"
+            , Method POST "user/{id}/comment/{article}"
+            ]
+            []
+            Nothing
+            Nothing
+    it "colon path" $ do
+      lookupMethod ( Request
+                     GET
+                     "user/:id"
+                     [("id", "1234")]
+                     []
+                     []
+                     ""
+                   ) service `shouldBe` Just (Method GET "user/:id") 
+    it "brace path" $ do
+      lookupMethod ( Request
+                     POST
+                     "user/{id}/comment/{article}"
+                     [("id", "1234"), ("article", "331")]
+                     []
+                     []
+                     ""
+                   ) service `shouldBe` Just (Method POST "user/{id}/comment/{article}")
+    it "parameter in path" $ do
+      lookupMethod ( Request
+                     GET
+                     "user/1234"
+                     []
+                     []
+                     []
+                     ""
+                   ) service `shouldBe` Just (Method GET "user/:id") 
+    it "mixed path" $ do
+      lookupMethod ( Request
+                     POST
+                     "user/{id}/comment/:article"
+                     [("id", "1234"), ("article", "331")]
+                     []
+                     []
+                     ""
+                   ) service `shouldBe` Just (Method POST "user/{id}/comment/{article}")
+    it "not exist" $ do
+      lookupMethod ( Request
+                     GET
+                     "nyaan/:aaa"
+                     []
+                     []
+                     []
+                     ""
+                   ) service `shouldBe` Nothing
+    it "invalid method" $ do
+      lookupMethod ( Request
+                     GET
+                     "user/{id}/comment/{article}"
+                     [("id", "1234"), ("article", "331")]
+                     []
+                     []
+                     ""
+                   ) service `shouldBe` Nothing
