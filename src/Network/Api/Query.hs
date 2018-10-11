9cf14598ff2740c039fb4da99a319fe5d72b6a87
Akihito KIRISAKI <kirisaki@klaraworks.net>
2018-10-11 18:51:28 +0900

    Add UrlEncoded

--- src/Network/Api/Query.hs
+++ src/Network/Api/Query.hs
@@ -10,20 +10,35 @@
 -----------------------------------------------------------------------------
 module Network.Api.Query
   ( Query
+  , urlEncode
+  , urlDecode
   ) where
 
-import Data.HashMap.Strict as HM
-import qualified Data.ByteString as BSS
-import qualified Data.Text as T
-import Network.HTTP.Types.URI hiding (Query)
+import qualified Data.ByteString        as BSS
+import           Data.HashMap.Strict    as HM
+import qualified Data.Text              as T
+import           Data.Text.Encoding
+import qualified Network.HTTP.Types.URI as U
 
 -- | Collection of URL query parameters.
 --   Behaviour when duplicated query keys at URL is not defined,
---   but this makes implements complecated, so treat keys as unique in this module.
+--   but it makes implements complecated, so treat keys as unique in this module.
+type Query = HM.HashMap UrlEncoded (Maybe UrlEncoded)
 
-type Query = HM.HashMap URIEncoded (Maybe URIEncoded)
+-- | URIEncoded 'ByteString'
+newtype UrlEncoded = UrlEncoded
+  { unUrlEncoded :: BSS.ByteString -- ^ Unwrap encoded string.
+  }
 
-type URIEncoded = BSS.ByteString
+-- | Encode utf-8 text to URI encoded bytestrings.
+--   It doesn't convert ' ' to '+'
+urlEncode :: T.Text -> UrlEncoded
+urlEncode = UrlEncoded . U.urlEncode False . encodeUtf8
+
+-- | Decode URI encoded bytestrings to utf-8 text.
+--   It doesn't convert '+' to ' '
+urlDecode :: UrlEncoded -> T.Text
+urlDecode = decodeUtf8 . U.urlDecode True . unUrlEncoded
 
 toQuery :: [(T.Text, Maybe T.Text)] -> Query
 toQuery kvs = undefined
